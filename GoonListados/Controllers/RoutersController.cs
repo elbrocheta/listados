f1872fa6372465b459acdc133902f66c7c095f42
David <dcarpioc@gmail.com>
2018-11-02 10:38:40 +0100

    Añadimos plantilla

---
+++ GoonListados/Controllers/RoutersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoonListados.Controllers
+{
+    public class RoutersController : Controller
+    {
+        // GET: Routers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GoonListados/Controllers/SimsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoonListados.Controllers
+{
+    public class SimsController : Controller
+    {
+        // GET: Sims
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
