c678f161bbc0d99c9fc39497d4cc3b87d4f43bff
David <dcarpioc@gmail.com>
2018-11-14 10:07:39 +0100

    Ajustes tablas

--- GoonListados/Controllers/SimsController.cs
+++ GoonListados/Controllers/SimsController.cs
@@ -2,6 +2,8 @@
 using GoonListados.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,7 +15,32 @@ namespace GoonListados.Controllers
         // GET: Sims
         public ActionResult Index()
         {
-            List<DataItem> _list = DataHelper.p_GetData("Sims");
+            //List<DataItem> _list = DataHelper.p_GetData("Sims");
+            List<DataItem> _list = new List<DataItem>();
+
+            String _s = ConfigurationManager.ConnectionStrings["ConnectionDefault"].ConnectionString;
+
+            SqlConnection _conn = new SqlConnection(_s);
+            SqlCommand _command = new SqlCommand("SELECT * FROM Noticias", _conn);
+
+            _conn.Open();           
+
+            SqlDataReader _dr = _command.ExecuteReader();
+
+            while (_dr.Read()) {
+
+                _list.Add(new DataItem() {
+                    datos1 =  _dr.GetValue(0).ToString(),
+                    datos2 = _dr.GetValue(1).ToString(),
+                    datos3 = _dr.GetValue(2).ToString(),
+                    datos4 = _dr.GetValue(3).ToString(),
+                    datos5 = "<< sin datos >>",
+                    datos6 = "<< sin datos >>"
+
+                });
+            }
+
+                _conn.Close();
 
             return View(_list);
         }
